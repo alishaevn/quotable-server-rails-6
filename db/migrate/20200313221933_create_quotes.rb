f74ca28618548821fcb583a74bb6061db81133eb
Alisha Evans <alisha@notch8.com>
2020-03-15 22:18:32 -0700

    remove the "date" column from the quotes table

--- db/migrate/20200313221933_create_quotes.rb
+++ db/migrate/20200313221933_create_quotes.rb
@@ -1,7 +1,6 @@
 class CreateQuotes < ActiveRecord::Migration[6.0]
   def change
     create_table :quotes do |t|
-      t.date :date
       t.string :author
       t.text :body
 
--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,8 @@
 ActiveRecord::Schema.define(version: 2020_03_13_221933) do
 
   create_table "quotes", force: :cascade do |t|
-    t.text "body"
     t.string "author"
-    t.date "date"
+    t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
