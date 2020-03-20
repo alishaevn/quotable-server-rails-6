84a343d7392d6a5036c248badb413c830d13d7ff
Alisha Evans <alisha@notch8.com>
2020-03-20 15:34:14 -0700

    set the default_url_options values and flash messages

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "quotable_server_rails_6_production"
 
   config.action_mailer.perform_caching = false
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 } // change the host if this ever goes live
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
