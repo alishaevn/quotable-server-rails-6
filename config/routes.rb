f836dba8bb18079dceb5ea1a4ba2b106800173da
Alisha Evans <alisha@notch8.com>
2020-03-20 21:59:39 -0700

    add more routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'quotes#index', as: 'home'
+
+  resources :quotes
+  resources :users, only: [:new, :create]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
   get 'sessions/new'
   get 'sessions/create'
   get 'sessions/login'
   get 'sessions/welcome'
   get 'users/new'
   get 'users/create'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'quotes#index', as: 'home'
-
-  resources :quotes
+  get 'welcome', to: 'sessions#welcome'
 end
