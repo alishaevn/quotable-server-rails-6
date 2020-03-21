c5a38f8a5b3f0ccf1aaa85e867a0fdb7f7cd89ea
Alisha Evans <alisha@notch8.com>
2020-03-21 16:56:12 -0700

    setup encoding and decoding jwt capabilities

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   root 'quotes#index', as: 'home'
 
   resources :quotes
+  resources :users, param: :_username
+
+  post '/auth/login', to: 'authentication#login'
+  get '/*a', to: 'application#not_found'
 end
